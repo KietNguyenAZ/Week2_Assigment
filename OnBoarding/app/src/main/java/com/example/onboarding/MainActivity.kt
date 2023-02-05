2980d993b415d64cb2e249be4029902d8cc9d03c
Kiet <trannguyentuankiet14112001@gmail.com>
2023-02-05 23:34:58 +0700

    No comment

--- OnBoarding/app/src/main/java/com/example/onboarding/MainActivity.kt
+++ OnBoarding/app/src/main/java/com/example/onboarding/MainActivity.kt
@@ -7,10 +7,13 @@ import android.os.Bundle
 import android.os.Handler
 import android.widget.Button
 import android.widget.ImageButton
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Thread.sleep(2000)
+        val splashScreen = installSplashScreen()
         setContentView(R.layout.activity_main)
         val button = findViewById<ImageButton>(R.id.move_screen)
         val it2 = Intent(this, Onboarding2::class.java)
